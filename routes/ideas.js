1227992f3c41c461e28c51b4758f6e20430c5978
KaniewskiSoftware <117393647+st4rkmano@users.noreply.github.com>
2023-04-17 14:02:57 +0200

    post method handled

--- routes/ideas.js
+++ routes/ideas.js
@@ -2,28 +2,28 @@ const express = require("express");
 const router = express.Router();
 
 const ideas = [
-    {
-      id: 1,
-      text: "Take out the trash",
-      tag: "home",
-      username: "admin",
-      date: "2020-01-01",
-    },
-    {
-      id: 2,
-      text: "Take out the trash",
-      tag: "home",
-      username: "admin",
-      date: "2020-01-01",
-    },
-    {
-      id: 3,
-      text: "Take out the trash",
-      tag: "home",
-      username: "admin",
-      date: "2020-01-01",
-    },
-  ];
+  {
+    id: 1,
+    text: "Take out the trash",
+    tag: "home",
+    username: "admin",
+    date: "2020-01-01",
+  },
+  {
+    id: 2,
+    text: "Take out the trash",
+    tag: "home",
+    username: "admin",
+    date: "2020-01-01",
+  },
+  {
+    id: 3,
+    text: "Take out the trash",
+    tag: "home",
+    username: "admin",
+    date: "2020-01-01",
+  },
+];
 
 // Get all ideas
 router.get("/", (req, res) => {
@@ -39,6 +39,21 @@ router.get("/:id", (req, res) => {
       .status(404)
       .json({ success: false, error: "Resource not found" });
   }
+
+  res.json({ success: true, data: idea });
+});
+
+// Create idea
+router.post("/", (req, res) => {
+  const idea = {
+    id: ideas.length + 1,
+    text: req.body.text,
+    tag: req.body.tag,
+    username: req.body.username,
+    date: new Date().toISOString().slice(0, 10),
+  };
+
+  ideas.push(idea);
 
   res.json({ success: true, data: idea });
 });
