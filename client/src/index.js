8ae4429979e3968b930da56de0e5addad01d59c9
KaniewskiSoftware <117393647+st4rkmano@users.noreply.github.com>
2023-04-18 09:18:41 +0200

    deployment prep

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,4 @@
-import "@fortawesome/fontawesome-free/css/all.min.css";
+// import "@fortawesome/fontawesome-free/css/all.min.css";
 import Modal from "./components/Modal";
 import IdeaForm from "./components/IdeaForm";
 import IdeaList from "./components/IdeaList";
